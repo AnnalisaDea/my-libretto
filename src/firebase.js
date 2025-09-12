42387a559da6fa0f400eadb548167905434fb420
Annalisa Deanesi <annalisa.deanesi@gmail.com>
2025-09-12 11:45:35 +0200

    Add initial project setup with vue, firebase, and tailwind. Configured project structure

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBO2e9HBU11oM7RsxWGZtrtFsLKW_971iM",
+    authDomain: "my-libretto.firebaseapp.com",
+    projectId: "my-libretto",
+    storageBucket: "my-libretto.firebasestorage.app",
+    messagingSenderId: "687246205179",
+    appId: "1:687246205179:web:318b63bc154d780d62d05e"
+};
+
+// Inizializza l'app di Firebase
+const app = initializeApp(firebaseConfig);
+
+// Esporta le istanze dei servizi che useremo
+export const auth = getAuth(app);
+export const db = getFirestore(app);
